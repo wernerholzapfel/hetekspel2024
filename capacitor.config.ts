22b5d2e28a720a8d6f23dde9bfe34f708e6daddd
Werner Holzapfel <werner.holzapfel@gmail.com>
2026-05-12 21:30:37 +0200

    Versie 4.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wernerholzapfel.hetekspel',
-  appName: 'Het EK Spel',
+  appName: 'Het WK Spel',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
